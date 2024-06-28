cd78d6aff1e52fbb97b201f22529aa8bbaeddd00
Cadu 79 <eduardoprimogenito@gmail.com>
2024-06-28 15:30:19 -0300

    alteração no campo vagas

--- src/components/Cabecalho/index.tsx
+++ src/components/Cabecalho/index.tsx
@@ -1,9 +1,20 @@
-import styles from './Cabecalho.module.css'
+import styled from 'styled-components';
+
+const Header = styled.header`
+  background-color: #f8f9fa;
+  padding: 20px;
+  text-align: center;
+`;
+
+const Title = styled.h1`
+  font-size: 2rem;
+  color: #343a40;
+`;
 
 const Cabecalho = () => (
-  <header className={styles.cabecalho}>
-    <h1>EBAC Jobs</h1>
-  </header>
-)
+  <Header>
+    <Title>EBAC Jobs</Title>
+  </Header>
+);
 
-export default Cabecalho
+export default Cabecalho;
